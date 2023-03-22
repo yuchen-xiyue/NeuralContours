831eeff144c5928667f432ba6c2275801bf82aaf
yuchen-xiyue <yw3283@bath.ac.uk>
2023-03-22 21:41:28 +0000

    m

--- scripts/image_branch.py
+++ scripts/image_branch.py
@@ -87,4 +87,4 @@ with torch.no_grad():
     IT_output_ridge = ridge_detection(IT_output_np, folder_pos, conf)
 
 show_img(IT_output_np*255., args.save_name)
-show_img(IT_output_ridge*255., args.save_name.split('.')[0]+'_ridge.png')
+show_img((1-IT_output_ridge)*255., args.save_name.split('.')[0]+'_ridge.png')
